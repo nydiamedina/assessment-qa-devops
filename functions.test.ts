6bf03ec819ec97766c3db34e8f608f6937a20c2a
Nydia Medina <nvmedina29@gmail.com>
2022-09-20 12:11:39 -0600

    Submitting Part 3 of Assessment

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,20 @@
-const {shuffleArray} = require('./utils')
+const { bots } = require('./data');
+const { shuffleArray } = require('./utils');
 
 describe('shuffleArray should', () => {
     // CODE HERE
-})
+    test('return an array', () => {
+        const shuffledArray = shuffleArray(bots);
+        expect(Array.isArray(shuffledArray)).toBeTruthy();
+    });
+
+    test('return an array of the same length as the argument', () => {
+        const shuffledArray = shuffleArray(bots);
+        expect(shuffledArray.length).toBe(bots.length);
+    });
+
+    test('return an array with the same items as the argument', () => {
+        const shuffledArray = shuffleArray(bots);
+        expect(shuffledArray).toEqual(expect.arrayContaining(bots));
+    });
+});
