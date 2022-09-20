90578e5d44e8665b633f3a66cbb44adde21e9644
Nydia Medina <nvmedina29@gmail.com>
2022-09-20 12:41:15 -0600

    Submitting Part 4

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,20 +1,43 @@
 
-import { Builder, Capabilities, By } from "selenium-webdriver"
+import { Builder, Capabilities, By } from "selenium-webdriver";
 
-require('chromedriver')
+require('chromedriver');
 
-const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
-})
+    driver.get('http://127.0.0.1:5500/public/index.html');
+});
 
 afterAll(async () => {
-    driver.quit()
-})
+    driver.quit();
+});
 
 test('Title shows up when page loads', async () => {
-    const title = await driver.findElement(By.id('title'))
-    const displayed = await title.isDisplayed()
-    expect(displayed).toBe(true)
-})
+    const title = await driver.findElement(By.id('title'));
+    const displayed = await title.isDisplayed();
+    expect(displayed).toBe(true);
+});
+
+test('Bot choices are displayed when clicking on Draw', async () => {
+    await driver.findElement(By.id('draw')).click();
+    await driver.sleep(2000);
+    
+    const choicesDiv = await driver.findElement(By.id('choices'));
+    const displayedChoices = await choicesDiv.isDisplayed();
+
+    expect(displayedChoices).toBe(true);
+});
+
+test('Player Duo is displayed when clicking on Add to Duo', async () => {
+    await driver.findElement(By.id('draw')).click();
+    await driver.sleep(2000);
+
+    await driver.findElement(By.xpath('//*[@id="choices"]/div[1]/button')).click();
+    await driver.sleep(2000);
+    
+    const playerDuoDiv = await driver.findElement(By.id('player-duo'));
+    const displayedPlayerDuo = await playerDuoDiv.isDisplayed();
+
+    expect(displayedPlayerDuo).toBe(true);
+});
